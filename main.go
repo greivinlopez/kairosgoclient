704ab3b77733e3bf0fe1258122d6ac456a163bc2
Greivin López <greivin.lopez@gmail.com>
2023-09-28 17:58:23 -0600

    decrypt and token request

--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,18 +20,43 @@ import (
 const POST = "POST"
 
 var (
-	baseURL  = os.Getenv("KAIROS_API_URL")
-	clientID = os.Getenv("KAIROS_CLIENT_ID")
-	//clientSecret = os.Getenv("KAIROS_CLIENT_SECRET")
+	baseURL      = os.Getenv("KAIROS_API_URL")
+	clientID     = os.Getenv("KAIROS_CLIENT_ID")
+	clientSecret = os.Getenv("KAIROS_CLIENT_SECRET")
 )
 
-type LoginResponse struct {
+type loginResponse struct {
 	Challenge string `json:"challenge"`
+}
+
+type tokenResponse struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int    `json:"expires_in"`
+	TokenType   string `json:"token_type"`
 }
 
 func prettyPrint(data map[string]interface{}) string {
 	jsondata, _ := json.MarshalIndent(data, "", "    ")
 	return string(jsondata)
+}
+
+func decrypt(data, priv string) (string, error) {
+	data2, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		return "", err
+	}
+
+	block, _ := pem.Decode([]byte(priv))
+	key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return "", err
+	}
+
+	decrypted, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, key, data2, nil)
+	if err != nil {
+		return "", err
+	}
+	return string(decrypted), nil
 }
 
 func login() (challenge string, err error) {
@@ -63,7 +94,7 @@ func login() (challenge string, err error) {
 		return "", err
 	}
 
-	var response LoginResponse
+	var response loginResponse
 	if err := json.Unmarshal(body, &response); err != nil {
 		return "", err
 	}
@@ -71,13 +102,73 @@ func login() (challenge string, err error) {
 	return response.Challenge, nil
 }
 
+func token(challenge string) (token string, err error) {
+	///Attempt to request a token: http://localhost:4055/api/v1/token
+	/*
+	   With payload:
+	     Here is your token: */
+	url, err := url.JoinPath(baseURL, "token")
+	if err != nil {
+		return "", err
+	}
+	fmt.Println("Attempt to request a token:", url)
+
+	data := map[string]interface{}{
+		"client_id":     clientID,
+		"client_secret": clientSecret,
+		"challenge":     challenge,
+	}
+	payload, err := json.Marshal(data)
+	if err != nil {
+		return "", err
+	}
+	j := prettyPrint(data)
+	fmt.Println("With payload:", j)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(POST, url, bytes.NewBuffer(payload))
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var response tokenResponse
+	if err := json.Unmarshal(body, &response); err != nil {
+		return "", err
+	}
+
+	return response.AccessToken, nil
+}
+
 func main() {
 	fmt.Println("Kairos API usage example in Go")
 	fmt.Println("------------------------------")
 
-	challenge, err := login()
+	// Perform the login
+	encryptedChallenge, err := login()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	fmt.Println("Challenge:", challenge)
+
+	// Decrypt the challenge
+	privateKey := os.Getenv("RSA_PRIVATE_KEY")
+	decryptedChallenge, err := decrypt(encryptedChallenge, privateKey)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// Ask for an access token
+	token, err := token(decryptedChallenge)
+	fmt.Println("Here is your token:", token)
 }
